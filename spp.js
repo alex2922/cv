0345e27c816b8cda653b9c2104f34fba2a9b1cf7
Harsh Singh <alexlin2922@gmail.com>
2023-05-15 16:28:15 +0530

    nav done

---
+++ spp.js
@@ -0,0 +1,17 @@
+// function navOpen(){
+//     document.querySelector('#open-button').style.display = 'none';
+//     document.querySelector('#nav-list').style.display = 'block';
+// }
+// function navClose(){
+//     document.querySelector('#open-button').style.display = 'block';
+//     document.querySelector('#nav-list').style.display = 'none';
+// }
+
+
+
+function navOpen(){
+    document.querySelector('#nav-list').style.display = 'block';
+}
+function navClose(){
+    document.querySelector('#nav-list').style.display = 'none';
+}
